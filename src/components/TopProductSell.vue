2724224dd0e1770efc2ad0b4dcaa352f30d9bfd3
Firstname Lastname <your_email@youremail.com>
2022-03-31 22:37:37 +0100

    g

--- src/components/TopProductSell.vue
+++ src/components/TopProductSell.vue
@@ -14,7 +14,7 @@
       <div class="col-12 col-md-4 p-5 mt-3">
         <a href="#"
           ><img
-            src="/images/Muscle_Fit_white.jfif"
+            src="images/Muscle_Fit_white.jfif"
             class="rounded-circle img-fluid border"
         /></a>
         <h5 class="text-center mt-3 mb-3">Collar Shirt</h5>
